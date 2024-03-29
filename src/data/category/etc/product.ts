6a07d5f186ab3e93ff78ddd14a856eb4035fa0e9
innovatorCho <spectre1c@gmail.com>
2024-03-29 13:21:28 +0900

    쇼핑몰 기본틀 만들기 완료

---
+++ src/data/category/etc/product.ts
@@ -0,0 +1,49 @@
+export const product = {
+    name: '파워큐브 오리지널 5구 멀티탭 그레이 1150/KRORPC',
+    info: [
+      {
+        site: 'coupang',
+        name: '쿠팡',
+        price: '23,500',
+        href: 'https://www.coupang.com/vp/products/7634574182?itemId=20270293690&vendorItemId=77985472538&isAddedCart=',
+      },
+      {
+        site: 'naver',
+        name: '네이버',
+        price: '24,400',
+        href: 'https://smartstore.naver.com/bareunmoongoo/products/9230162080?NaPm=ct%3Dluau6w74%7Cci%3Dc7781f57d3aef0870415261a52b239c463849eec%7Ctr%3Dslsl%7Csn%3D8932056%7Chk%3Db4e594301717f3289537002cd0f0e2dcb044d508',
+      },
+    ],
+    breadcrumbs: [
+      { id: 1, name: '홈', href: '/' },
+      { id: 2, name: 'all', href: '/' },
+    ],
+    images: [
+      {
+        src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg',
+        alt: 'Two each of gray, white, and black shirts laying flat.',
+      },
+      {
+        src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg',
+        alt: 'Model wearing plain black basic tee.',
+      },
+      {
+        src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg',
+        alt: 'Model wearing plain gray basic tee.',
+      },
+      {
+        src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg',
+        alt: 'Model wearing plain white basic tee.',
+      },
+    ],
+    description:
+      'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+    highlights: [
+      'Hand cut and sewn locally',
+      'Dyed with our proprietary colors',
+      'Pre-washed & pre-shrunk',
+      'Ultra-soft 100% cotton',
+    ],
+    details:
+      'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+  }
---
+++ src/data/menu.ts
@@ -0,0 +1,17 @@
+
+import { ChartPieIcon } from '@heroicons/react/24/outline';
+
+export const products = [
+    { name: 'All', description: '전체상품 정보', href: '/', icon: ChartPieIcon },
+    /*
+    { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+    { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
+    { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+    { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+    */
+];
+
+// <a href="#" className="text-sm font-semibold leading-6 text-gray-900">About</a>         
+export const menuItems = [
+    { name: "About", description: '사이트 정보', href: '/about'},
+]
